6984466e80bb7c1d9f89bfe54373640c6febfa00
huuthangcv0612 <huuthang.cv0612@gmail.com>
2026-07-27 11:18:46 +0700

    release v1.1.0

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ const authLimiter = rateLimit({
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://vocab-app-fe-five.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
